eddaf1fb361d52fd4cf1fdd59347e3bc5bfd2943
WacoderForever <sethomondi088@gmail.com>
2023-05-10 01:39:31 +0300

    tat

--- main.c
+++ main.c
@@ -14,4 +14,4 @@ struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
             );
 }
 
-CWEB_START_MACRO(5001, main_sever)
+CWEB_START_MACRO(5003, main_sever)
--- render_interface.c
+++ render_interface.c
@@ -8,14 +8,15 @@ struct CTextStack * render_interface(){
         s->open(s,HEAD);
         s->close(s,HEAD);
         s->open(s,BODY);
-
-                s->auto$close(s,INPUT,R"( name="num1" )");
-                s->auto$close(s,BR,"");
-                    s->auto$close(s,INPUT,R"( name="num2" )");
-                s->auto$close(s,BR,"");
-                s->open(s,BUTTON);
-                    s->segment_text(s,"Click Here");
-                s->close(s,BUTTON);
+                    s->$open(s,FORM,R"(action="/calculate")");
+                        s->auto$close(s,INPUT,R"( name="num1"placeholder='num1' )");
+                        s->auto$close(s,BR,"");
+                            s->auto$close(s,INPUT,R"( name="num2"placeholder='num2' )");
+                        s->auto$close(s,BR,"");
+                        s->open(s,BUTTON);
+                            s->segment_text(s,"Click Here");
+                        s->close(s,BUTTON);
+                    s->$close(s,FORM);
         s->close(s,BODY);
     s->close(s,HTML);
     return s;
