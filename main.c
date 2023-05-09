c718ef05d0e2ad376f292dd0bb1b6e5b80fbbc2e
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-09 19:46:09 -0300

    att

--- main.c
+++ main.c
@@ -6,8 +6,20 @@
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
+    char *route = request->route;
+
+    //means that the button were cliced
+    if(strcmp(route,"/calculate") == 0){
+            char *num1 = request->get_param(request,"num1");
+            char *num2 = request->get_param(request,"num2");
+            printf("-----------------------------\n");
+            printf("num1 : %s\n",num1);
+            printf("num2 : %s\n",num2);
+    }
+
 
     struct CTextStack *stack = render_interface();
+
     cweb_send_rendered_CTextStack_cleaning_memory(
                 stack,
                 200
--- render_interface.c
+++ render_interface.c
@@ -3,20 +3,19 @@
 struct CTextStack * render_interface(){
     struct CTextStack *s = newCTextStack(CTEXT_LINE_BREAKER, CTEXT_SEPARATOR);
 
-
     s->$open(s,HTML,"lang=\"en\"");
         s->open(s,HEAD);
         s->close(s,HEAD);
         s->open(s,BODY);
                     s->$open(s,FORM,R"(action="/calculate")");
-                        s->auto$close(s,INPUT,R"( name="num1"placeholder='num1' )");
+                        s->auto$close(s,INPUT,R"( name="num1" placeholder='num1' )");
                         s->auto$close(s,BR,"");
                             s->auto$close(s,INPUT,R"( name="num2"placeholder='num2' )");
                         s->auto$close(s,BR,"");
                         s->open(s,BUTTON);
                             s->segment_text(s,"Click Here");
                         s->close(s,BUTTON);
-                    s->$close(s,FORM);
+                    s->close(s,FORM);
         s->close(s,BODY);
     s->close(s,HTML);
     return s;
