5aa7e12ec57262d0a696344f92bfbab5fc233944
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-09 17:50:36 -0300

    attt

--- main.c
+++ main.c
@@ -1,1 +1,9 @@
-#include <stdio.h>
+
+#include "dependencies/CWebStudio.h"
+
+struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
+
+   cweb_send_text("aaaa",200);
+}
+
+CWEB_START_MACRO(5000, main_sever)
