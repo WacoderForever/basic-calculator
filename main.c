4bf09ae3d80e43cd576b185a2fd350888a326c74
WacoderForever <sethomondi088@gmail.com>
2023-05-10 03:33:33 +0300

    as

--- main.c
+++ main.c
@@ -13,8 +13,38 @@ struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
             char *num1 = request->get_param(request,"num1");
             char *num2 = request->get_param(request,"num2");
             char *operator = request->get_param(request,"operator");
-            char *result="THG";
-            struct CTextStack *stack = render_interface(result);
+            char *result;
+            //transform num1 to int
+            int num1_int=atoi(num1);
+            //transform num2 to int;
+            int num2_int=atoi(num2);
+            //transform result to int
+            int result_int=atoi(result);
+            if(operator=='+'){
+               result_int=num1_int+num2_int;
+               char text[30];
+               sprintf(text,"The result is %d",result_int); 
+               struct CTextStack *stack = render_interface(text);
+            }
+            if(operator=='-'){
+               result_int=num1_int-num2_int; 
+               char text[30];
+               sprintf(text,"The result is %d",result_int); 
+               struct CTextStack *stack = render_interface(text);
+            }
+            if(operator=='x'){
+               result_int=num1_int*num2_int; 
+               char text[30];
+               sprintf(text,"The result is %d",result_int); 
+               struct CTextStack *stack = render_interface(text);
+            }
+            if(operator=='/'){
+               result_int=num1_int/num2_int; 
+               char text[30];
+               sprintf(text,"The result is %d",result_int); 
+               struct CTextStack *stack = render_interface(text);
+            }
+            
 
         return cweb_send_rendered_CTextStack_cleaning_memory(
                 stack,
@@ -31,4 +61,4 @@ struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
             );
 }
 
-CWEB_START_MACRO(5006, main_sever)
+CWEB_START_MACRO(5008, main_sever)
