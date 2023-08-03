1e5329b4e217e2a8207d53e0bec7563946e562a3
Wacoder Forever <sethomondi088@gmail.com>
2023-08-03 03:32:04 +0300

    yt

--- routes/trigonometric_calculate_route.c
+++ routes/trigonometric_calculate_route.c
@@ -1,4 +1,4 @@
-
+#include <math.h>
 double factorial(int n) {
     if (n <= 1)
         return 1;
@@ -13,30 +13,6 @@ double power(double base, int exponent) {
     return result;
 }
 
-double sin(double x) {
-    double result = 0.0;
-    int sign = 1;
-    for (int n = 1; n <= 10; n += 2) {
-        result += sign * power(x, n) / factorial(n);
-        sign *= -1;
-    }
-    return result;
-}
-
-double cos(double x) {
-    double result = 0.0;
-    int sign = 1;
-    for (int n = 0; n <= 10; n += 2) {
-        result += sign * power(x, n) / factorial(n);
-        sign *= -1;
-    }
-    return result;
-}
-
-double tan(double x) {
-    return sin(x) / cos(x);
-}
-
 struct CwebHttpResponse * trigonometric_calculate_route(struct CwebHttpRequest *request){
 
 
@@ -49,19 +25,20 @@ struct CwebHttpResponse * trigonometric_calculate_route(struct CwebHttpRequest *
 
     //transform result to int
     double result_int;
+    double deg=57.29577951;
 
     if(strcmp(operator,"SIN")== 0){
-        result_int = sin(angle_float);
+        result_int = sin(angle_float/deg);
     }
     if(strcmp(operator,"COS")== 0){
-        result_int = cos(angle_float);
+        result_int = cos(angle_float/deg);
     }
     if(strcmp(operator,"TAN")== 0){
-        result_int = tan (angle_float);
+        result_int = tan (angle_float/deg);
     }
 
     char result[30];
-    sprintf(result,"The Result is:<br>%f",result_int);
+    sprintf(result,"The Result is:<br>%.3f",result_int);
     struct CTextStack *stack = render_trigonometric_calculator(result);
     return cweb_send_rendered_CTextStack_cleaning_memory(
             stack,
